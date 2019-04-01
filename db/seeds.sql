24751413d81865667a228206d7780f57c030c5ac
Andrem <andreaif4495@gmail.com>
2019-03-31 20:43:19 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE `burgers_db`;
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Cheese burger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Double burger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Krabby Patty', false);
